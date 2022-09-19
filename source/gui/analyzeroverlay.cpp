4f04f3463fcb9f05a5d20d7e054808d61fdbd40e
m-aiken <mattaiken91@gmail.com>
2022-09-19 14:08:53 +0100

    analyzer overlay is now mapping x coord to frequency properly

--- source/gui/analyzeroverlay.cpp
+++ source/gui/analyzeroverlay.cpp
@@ -1,5 +1,6 @@
 #include "analyzeroverlay.h"
 #include "../utils/colourpalette.h"
+#include "../utils/globals.h"
 
 //==============================================================================
 void AnalyzerOverlay::paint(juce::Graphics& g)
@@ -16,5 +17,26 @@ void AnalyzerOverlay::resized()
 
 void AnalyzerOverlay::mouseDrag(const juce::MouseEvent& event)
 {
+    auto boundsWidth = fftBoundingBox.getWidth();
+    auto boundsX = fftBoundingBox.getX();
+    auto boundsY = fftBoundingBox.getY();
+    auto boundsRight = fftBoundingBox.getRight();
+    auto responseCurveMin = fftBoundingBox.getBottom();
+    auto responseCurveMax = fftBoundingBox.getY();
 
+    auto xCoord = event.position.getX() - boundsX;
+    auto yCoord = event.position.getY() - boundsY;
+
+    auto xFrequency = mapToLog10<double>(static_cast<double>(xCoord) / boundsWidth,
+                                         Globals::getMinFrequency(),
+                                         Globals::getMaxFrequency());
+
+    // map y coord to magnitude, then gert ??? (the value to update the gain param) from magnitude
+
+    if (xFrequency >= Globals::getMinFrequency() && xFrequency <= Globals::getMaxFrequency()) {
+        // update frequency params
+    }
+
+    DBG(juce::String("x: ") << xFrequency);
+    DBG(juce::String("y: ") << yCoord);
 }

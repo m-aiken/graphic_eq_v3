44bc73e53a3330a8c35567e3d47b47d9b034cfd3
m-aiken <mattaiken91@gmail.com>
2023-02-05 14:14:11 +0000

    added tailwind colours

---
+++ source/utils/tailwindcolours.h
@@ -0,0 +1,251 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+namespace TailwindColours {
+
+// Red
+static const juce::Colour Red900       = juce::Colour(127, 29, 29);
+static const juce::Colour Red800       = juce::Colour(153, 27, 27);
+static const juce::Colour Red700       = juce::Colour(185, 28, 28);
+static const juce::Colour Red600       = juce::Colour(220, 38, 38);
+static const juce::Colour Red500       = juce::Colour(239, 68, 68);
+static const juce::Colour Red400       = juce::Colour(248, 113, 113);
+static const juce::Colour Red300       = juce::Colour(252, 165, 165);
+static const juce::Colour Red200       = juce::Colour(254, 202, 202);
+static const juce::Colour Red100       = juce::Colour(254, 226, 226);
+static const juce::Colour Red50        = juce::Colour(254, 242, 242);
+// Orange
+static const juce::Colour Orange900    = juce::Colour(124, 45, 18);
+static const juce::Colour Orange800    = juce::Colour(154, 52, 18);
+static const juce::Colour Orange700    = juce::Colour(194, 65, 12);
+static const juce::Colour Orange600    = juce::Colour(234, 88, 12);
+static const juce::Colour Orange500    = juce::Colour(249, 115, 22);
+static const juce::Colour Orange400    = juce::Colour(251, 146, 60);
+static const juce::Colour Orange300    = juce::Colour(253, 186, 116);
+static const juce::Colour Orange200    = juce::Colour(254, 215, 170);
+static const juce::Colour Orange100    = juce::Colour(255, 237, 213);
+static const juce::Colour Orange50     = juce::Colour(255, 247, 237);
+// Amber
+static const juce::Colour Amber900     = juce::Colour(120, 53, 15);
+static const juce::Colour Amber800     = juce::Colour(146, 64, 14);
+static const juce::Colour Amber700     = juce::Colour(180, 83, 9);
+static const juce::Colour Amber600     = juce::Colour(217, 119, 6);
+static const juce::Colour Amber500     = juce::Colour(245, 158, 11);
+static const juce::Colour Amber400     = juce::Colour(251, 191, 36);
+static const juce::Colour Amber300     = juce::Colour(252, 211, 77);
+static const juce::Colour Amber200     = juce::Colour(253, 230, 138);
+static const juce::Colour Amber100     = juce::Colour(254, 243, 199);
+static const juce::Colour Amber50      = juce::Colour(255, 251, 235);
+// Yellow
+static const juce::Colour Yellow900    = juce::Colour(113, 63, 18);
+static const juce::Colour Yellow800    = juce::Colour(133, 77, 14);
+static const juce::Colour Yellow700    = juce::Colour(161, 98, 7);
+static const juce::Colour Yellow600    = juce::Colour(202, 138, 4);
+static const juce::Colour Yellow500    = juce::Colour(234, 179, 8);
+static const juce::Colour Yellow400    = juce::Colour(250, 204, 21);
+static const juce::Colour Yellow300    = juce::Colour(253, 224, 71);
+static const juce::Colour Yellow200    = juce::Colour(254, 240, 138);
+static const juce::Colour Yellow100    = juce::Colour(254, 249, 195);
+static const juce::Colour Yellow50     = juce::Colour(254, 252, 232);
+// Lime
+static const juce::Colour Lime900      = juce::Colour(54, 83, 20);
+static const juce::Colour Lime800      = juce::Colour(63, 98, 18);
+static const juce::Colour Lime700      = juce::Colour(77, 124, 15);
+static const juce::Colour Lime600      = juce::Colour(101, 163, 13);
+static const juce::Colour Lime500      = juce::Colour(132, 204, 22);
+static const juce::Colour Lime400      = juce::Colour(163, 230, 53);
+static const juce::Colour Lime300      = juce::Colour(190, 242, 100);
+static const juce::Colour Lime200      = juce::Colour(217, 249, 157);
+static const juce::Colour Lime100      = juce::Colour(236, 252, 203);
+static const juce::Colour Lime50       = juce::Colour(247, 254, 231);
+// Green
+static const juce::Colour Green900     = juce::Colour(20, 83, 45);
+static const juce::Colour Green800     = juce::Colour(22, 101, 52);
+static const juce::Colour Green700     = juce::Colour(21, 128, 61);
+static const juce::Colour Green600     = juce::Colour(22, 163, 74);
+static const juce::Colour Green500     = juce::Colour(34, 197, 94);
+static const juce::Colour Green400     = juce::Colour(74, 222, 128);
+static const juce::Colour Green300     = juce::Colour(134, 239, 172);
+static const juce::Colour Green200     = juce::Colour(187, 247, 208);
+static const juce::Colour Green100     = juce::Colour(220, 252, 231);
+static const juce::Colour Green50      = juce::Colour(240, 253, 244);
+// Emerald
+static const juce::Colour Emerald900   = juce::Colour(6, 78, 59);
+static const juce::Colour Emerald800   = juce::Colour(6, 95, 70);
+static const juce::Colour Emerald700   = juce::Colour(4, 120, 87);
+static const juce::Colour Emerald600   = juce::Colour(5, 150, 105);
+static const juce::Colour Emerald500   = juce::Colour(16, 185, 129);
+static const juce::Colour Emerald400   = juce::Colour(52, 211, 153);
+static const juce::Colour Emerald300   = juce::Colour(110, 231, 183);
+static const juce::Colour Emerald200   = juce::Colour(167, 243, 208);
+static const juce::Colour Emerald100   = juce::Colour(209, 250, 229);
+static const juce::Colour Emerald50    = juce::Colour(236, 253, 245);
+// Teal
+static const juce::Colour Teal900      = juce::Colour(19, 78, 74);
+static const juce::Colour Teal800      = juce::Colour(17, 94, 89);
+static const juce::Colour Teal700      = juce::Colour(15, 118, 110);
+static const juce::Colour Teal600      = juce::Colour(13, 148, 136);
+static const juce::Colour Teal500      = juce::Colour(20, 184, 166);
+static const juce::Colour Teal400      = juce::Colour(45, 212, 191);
+static const juce::Colour Teal300      = juce::Colour(94, 234, 212);
+static const juce::Colour Teal200      = juce::Colour(153, 246, 228);
+static const juce::Colour Teal100      = juce::Colour(204, 251, 241);
+static const juce::Colour Teal50       = juce::Colour(240, 253, 250);
+// Cyan
+static const juce::Colour Cyan900      = juce::Colour(22, 78, 99);
+static const juce::Colour Cyan800      = juce::Colour(21, 94, 117);
+static const juce::Colour Cyan700      = juce::Colour(14, 116, 144);
+static const juce::Colour Cyan600      = juce::Colour(8, 145, 178);
+static const juce::Colour Cyan500      = juce::Colour(6, 182, 212);
+static const juce::Colour Cyan400      = juce::Colour(34, 211, 238);
+static const juce::Colour Cyan300      = juce::Colour(103, 232, 249);
+static const juce::Colour Cyan200      = juce::Colour(165, 243, 252);
+static const juce::Colour Cyan100      = juce::Colour(207, 250, 254);
+static const juce::Colour Cyan50       = juce::Colour(236, 254, 255);
+// Light Blue
+static const juce::Colour LightBlue900 = juce::Colour(12, 74, 110);
+static const juce::Colour LightBlue800 = juce::Colour(7, 89, 133);
+static const juce::Colour LightBlue700 = juce::Colour(3, 105, 161);
+static const juce::Colour LightBlue600 = juce::Colour(2, 132, 199);
+static const juce::Colour LightBlue500 = juce::Colour(14, 165, 233);
+static const juce::Colour LightBlue400 = juce::Colour(56, 189, 248);
+static const juce::Colour LightBlue300 = juce::Colour(125, 211, 252);
+static const juce::Colour LightBlue200 = juce::Colour(186, 230, 253);
+static const juce::Colour LightBlue100 = juce::Colour(224, 242, 254);
+static const juce::Colour LightBlue50  = juce::Colour(240, 249, 255);
+// Blue
+static const juce::Colour Blue900      = juce::Colour(30, 58, 138);
+static const juce::Colour Blue800      = juce::Colour(30, 64, 175);
+static const juce::Colour Blue700      = juce::Colour(29, 78, 216);
+static const juce::Colour Blue600      = juce::Colour(37, 99, 235);
+static const juce::Colour Blue500      = juce::Colour(59, 130, 246);
+static const juce::Colour Blue400      = juce::Colour(96, 165, 250);
+static const juce::Colour Blue300      = juce::Colour(147, 197, 253);
+static const juce::Colour Blue200      = juce::Colour(191, 219, 254);
+static const juce::Colour Blue100      = juce::Colour(219, 234, 254);
+static const juce::Colour Blue50       = juce::Colour(239, 246, 255);
+// Indigo
+static const juce::Colour Indigo900    = juce::Colour(49, 46, 129);
+static const juce::Colour Indigo800    = juce::Colour(55, 48, 163);
+static const juce::Colour Indigo700    = juce::Colour(67, 56, 202);
+static const juce::Colour Indigo600    = juce::Colour(79, 70, 229);
+static const juce::Colour Indigo500    = juce::Colour(99, 102, 241);
+static const juce::Colour Indigo400    = juce::Colour(129, 140, 248);
+static const juce::Colour Indigo300    = juce::Colour(165, 180, 252);
+static const juce::Colour Indigo200    = juce::Colour(199, 210, 254);
+static const juce::Colour Indigo100    = juce::Colour(224, 231, 255);
+static const juce::Colour Indigo50     = juce::Colour(238, 242, 255);
+// Violet
+static const juce::Colour Violet900    = juce::Colour(76, 29, 149);
+static const juce::Colour Violet800    = juce::Colour(91, 33, 182);
+static const juce::Colour Violet700    = juce::Colour(109, 40, 217);
+static const juce::Colour Violet600    = juce::Colour(124, 58, 237);
+static const juce::Colour Violet500    = juce::Colour(139, 92, 246);
+static const juce::Colour Violet400    = juce::Colour(167, 139, 250);
+static const juce::Colour Violet300    = juce::Colour(196, 181, 253);
+static const juce::Colour Violet200    = juce::Colour(221, 214, 254);
+static const juce::Colour Violet100    = juce::Colour(237, 233, 254);
+static const juce::Colour Violet50     = juce::Colour(245, 243, 255);
+// Purple
+static const juce::Colour Purple900    = juce::Colour(88, 28, 135);
+static const juce::Colour Purple800    = juce::Colour(107, 33, 168);
+static const juce::Colour Purple700    = juce::Colour(126, 34, 206);
+static const juce::Colour Purple600    = juce::Colour(147, 51, 234);
+static const juce::Colour Purple500    = juce::Colour(168, 85, 247);
+static const juce::Colour Purple400    = juce::Colour(192, 132, 252);
+static const juce::Colour Purple300    = juce::Colour(216, 180, 254);
+static const juce::Colour Purple200    = juce::Colour(233, 213, 255);
+static const juce::Colour Purple100    = juce::Colour(243, 232, 255);
+static const juce::Colour Purple50     = juce::Colour(250, 245, 255);
+// Fuchsia
+static const juce::Colour Fuchsia900   = juce::Colour(112, 26, 117);
+static const juce::Colour Fuchsia800   = juce::Colour(134, 25, 143);
+static const juce::Colour Fuchsia700   = juce::Colour(162, 28, 175);
+static const juce::Colour Fuchsia600   = juce::Colour(192, 38, 211);
+static const juce::Colour Fuchsia500   = juce::Colour(217, 70, 239);
+static const juce::Colour Fuchsia400   = juce::Colour(232, 121, 249);
+static const juce::Colour Fuchsia300   = juce::Colour(240, 171, 252);
+static const juce::Colour Fuchsia200   = juce::Colour(245, 208, 254);
+static const juce::Colour Fuchsia100   = juce::Colour(250, 232, 255);
+static const juce::Colour Fuchsia50    = juce::Colour(253, 244, 255);
+// Pink
+static const juce::Colour Pink900      = juce::Colour(131, 24, 67);
+static const juce::Colour Pink800      = juce::Colour(157, 23, 77);
+static const juce::Colour Pink700      = juce::Colour(190, 24, 93);
+static const juce::Colour Pink600      = juce::Colour(219, 39, 119);
+static const juce::Colour Pink500      = juce::Colour(236, 72, 153);
+static const juce::Colour Pink400      = juce::Colour(244, 114, 182);
+static const juce::Colour Pink300      = juce::Colour(249, 168, 212);
+static const juce::Colour Pink200      = juce::Colour(251, 207, 232);
+static const juce::Colour Pink100      = juce::Colour(252, 231, 243);
+static const juce::Colour Pink50       = juce::Colour(253, 242, 248);
+// Rose
+static const juce::Colour Rose900      = juce::Colour(136, 19, 55);
+static const juce::Colour Rose800      = juce::Colour(159, 18, 57);
+static const juce::Colour Rose700      = juce::Colour(190, 18, 60);
+static const juce::Colour Rose600      = juce::Colour(225, 29, 72);
+static const juce::Colour Rose500      = juce::Colour(244, 63, 94);
+static const juce::Colour Rose400      = juce::Colour(251, 113, 133);
+static const juce::Colour Rose300      = juce::Colour(253, 164, 175);
+static const juce::Colour Rose200      = juce::Colour(254, 205, 211);
+static const juce::Colour Rose100      = juce::Colour(255, 228, 230);
+static const juce::Colour Rose50       = juce::Colour(255, 241, 242);
+// Warm Gray
+static const juce::Colour WarmGray900  = juce::Colour(28, 25, 23);
+static const juce::Colour WarmGray800  = juce::Colour(41, 37, 36);
+static const juce::Colour WarmGray700  = juce::Colour(68, 64, 60);
+static const juce::Colour WarmGray600  = juce::Colour(87, 83, 78);
+static const juce::Colour WarmGray500  = juce::Colour(120, 113, 108);
+static const juce::Colour WarmGray400  = juce::Colour(168, 162, 158);
+static const juce::Colour WarmGray300  = juce::Colour(214, 211, 209);
+static const juce::Colour WarmGray200  = juce::Colour(231, 229, 228);
+static const juce::Colour WarmGray100  = juce::Colour(245, 245, 244);
+static const juce::Colour WarmGray50   = juce::Colour(250, 250, 249);
+// True Gray
+static const juce::Colour TrueGray900  = juce::Colour(23, 23, 23);
+static const juce::Colour TrueGray800  = juce::Colour(38, 38, 38);
+static const juce::Colour TrueGray700  = juce::Colour(64, 64, 64);
+static const juce::Colour TrueGray600  = juce::Colour(82, 82, 82);
+static const juce::Colour TrueGray500  = juce::Colour(115, 115, 115);
+static const juce::Colour TrueGray400  = juce::Colour(163, 163, 163);
+static const juce::Colour TrueGray300  = juce::Colour(212, 212, 212);
+static const juce::Colour TrueGray200  = juce::Colour(229, 229, 229);
+static const juce::Colour TrueGray100  = juce::Colour(245, 245, 245);
+static const juce::Colour TrueGray50   = juce::Colour(250, 250, 250);
+// Gray
+static const juce::Colour Gray900      = juce::Colour(24, 24, 27);
+static const juce::Colour Gray800      = juce::Colour(39, 39, 42);
+static const juce::Colour Gray700      = juce::Colour(63, 63, 70);
+static const juce::Colour Gray600      = juce::Colour(82, 82, 91);
+static const juce::Colour Gray500      = juce::Colour(113, 113, 122);
+static const juce::Colour Gray400      = juce::Colour(161, 161, 170);
+static const juce::Colour Gray300      = juce::Colour(212, 212, 216);
+static const juce::Colour Gray200      = juce::Colour(228, 228, 231);
+static const juce::Colour Gray100      = juce::Colour(244, 244, 245);
+static const juce::Colour Gray50       = juce::Colour(250, 250, 250);
+// Cool Gray
+static const juce::Colour CoolGray900  = juce::Colour(17, 24, 39);
+static const juce::Colour CoolGray800  = juce::Colour(31, 41, 55);
+static const juce::Colour CoolGray700  = juce::Colour(55, 65, 81);
+static const juce::Colour CoolGray600  = juce::Colour(75, 85, 99);
+static const juce::Colour CoolGray500  = juce::Colour(107, 114, 128);
+static const juce::Colour CoolGray400  = juce::Colour(156, 163, 175);
+static const juce::Colour CoolGray300  = juce::Colour(209, 213, 219);
+static const juce::Colour CoolGray200  = juce::Colour(229, 231, 235);
+static const juce::Colour CoolGray100  = juce::Colour(243, 244, 246);
+static const juce::Colour CoolGray50   = juce::Colour(249, 250, 251);
+// Blue Gray
+static const juce::Colour BlueGray900  = juce::Colour(15, 23, 42);
+static const juce::Colour BlueGray800  = juce::Colour(30, 41, 59);
+static const juce::Colour BlueGray700  = juce::Colour(51, 65, 85);
+static const juce::Colour BlueGray600  = juce::Colour(71, 85, 105);
+static const juce::Colour BlueGray500  = juce::Colour(100, 116, 139);
+static const juce::Colour BlueGray400  = juce::Colour(148, 163, 184);
+static const juce::Colour BlueGray300  = juce::Colour(203, 213, 225);
+static const juce::Colour BlueGray200  = juce::Colour(226, 232, 240);
+static const juce::Colour BlueGray100  = juce::Colour(241, 245, 249);
+static const juce::Colour BlueGray50   = juce::Colour(248, 250, 252);
+
+}
